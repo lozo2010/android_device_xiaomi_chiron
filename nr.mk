3a5d82be0a8246ba296b71acd0b176b4787c3bca
David <davidlglozo@gmail.com>
2018-05-19 15:32:02 +0200

    Nuclearize tree

--- nr.mk
+++ nr.mk
@@ -1,13 +1,15 @@
 $(call inherit-product, device/xiaomi/chiron/full_chiron.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/nr/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_chiron
+PRODUCT_NAME := nr_chiron
 PRODUCT_DEVICE := chiron
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2
 PRODUCT_MANUFACTURER := Xiaomi
+
+NR_BUILDTYPE := Official
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
